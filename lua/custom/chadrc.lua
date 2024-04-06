e90d119b943801661f580b51c2b3903e7830626c
Monish Sharma <107314033+MONISHSHARMA080@users.noreply.github.com>
2024-04-06 11:06:23 +0530

    Update chadrc.lua

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,4 +5,17 @@ M.ui = { theme = 'catppuccin' }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
+
+local disabled_plugins = require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins
+
+M.lazy_nvim = {
+    performance = {
+           rtp = {
+             disabled_plugins = vim.tbl_filter(function(x)
+                               return x ~= "tutor"
+                                     end, disabled_plugins),
+              },
+    },
+}
+
 return M
